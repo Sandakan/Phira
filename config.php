a890c333bb10b32bf9fb90005fd0684d1a27b79d
Sandakan Nipunajith <45460443+Sandakan@users.noreply.github.com>
2024-11-15 19:20:10 +0530

    fixed config

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('BASE_URL', 'http://localhost:80/Phira');
-$BASE_URL = BASE_URL;
-
-// Database credentials
-define('DATABASE_HOST_NAME', 'localhost');
-define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', 'root');
-define('DATABASE_NAME', 'PhiraDB');
