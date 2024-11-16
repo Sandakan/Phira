c993e215416b918704706f87cbbdfe6b0ae4cf58
sanujamenath <sanujamenath@gmail.com>
2024-11-16 23:38:18 +0530

    Biography and Email Verification

--- pages/onboarding/preferences.php
+++ pages/onboarding/preferences.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION["user_id"])) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Join With Us - Phira</title>
+    <title>User Preferences - Phira</title>
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/styles/styles.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/styles/fonts.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/styles/auth.css">
