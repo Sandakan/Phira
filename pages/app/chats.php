0fd4a95b9becc1e87599d89cffaf6810551a17b8
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2024-11-23 13:20:06 +0530

    chat panal

--- pages/app/chats.php
+++ pages/app/chats.php
@@ -6,10 +6,10 @@ require '../../utils/authenticate.php';
 $conn = initialize_database();
 session_start();
 
-authenticate(array("USER"));
-if (!isset($_SESSION["onboarding_completed"])) {
-    header("Location: " . BASE_URL . "/login.php");
-}
+// authenticate(array("USER"));
+// if (!isset($_SESSION["onboarding_completed"])) {
+//     header("Location: " . BASE_URL . "/login.php");
+// }
 
 ?>
 
@@ -22,14 +22,33 @@ if (!isset($_SESSION["onboarding_completed"])) {
     <title>Chats - Phira</title>
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/styles/styles.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/styles/sidebar.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/styles/chats.css">
     <link rel="shortcut icon" href="<?php echo BASE_URL; ?>/public/images/logo.webp" type="image/x-icon">
 </head>
 
 <body>
     <?php include('../../components/sidebar.php') ?>
-    <div>
+    <nav class="chats">
+        <h1>Chats</h1>
+        <div class="chats-container">
 
-    </div>
+            <div class="search-bar">
+                <span class="icons material-symbols-rounded">search</span>
+                <p>search</p>
+            </div>
+            <div class="chat-profile">
+                <img src="<?php echo BASE_URL; ?>/public/images/feedbackUser.png" alt="" class="profile-img">
+                <div class="chat-info-container">
+                    <div class="profile-info">
+                        <h2>Tiana</h2>
+                        <p>😂😂😂</p>
+                    </div>
+                    <p>12:00</p>
+                </div>
+            </div>
+
+        </div>
+    </nav>
 </body>
 
 </html>
