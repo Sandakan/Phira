e25d29639adeee21d24de1f08544dab4a2d7089b
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2024-11-23 13:52:16 +0530

    chat panal changes

--- pages/app/chats.php
+++ pages/app/chats.php
@@ -36,17 +36,69 @@ session_start();
                 <span class="icons material-symbols-rounded">search</span>
                 <p>search</p>
             </div>
-            <div class="chat-profile">
-                <img src="<?php echo BASE_URL; ?>/public/images/feedbackUser.png" alt="" class="profile-img">
-                <div class="chat-info-container">
-                    <div class="profile-info">
-                        <h2>Tiana</h2>
-                        <p>😂😂😂</p>
+            <div class="chats-list">
+                <div class="chat-profile">
+                    <img src="<?php echo BASE_URL; ?>/public/images/feedbackUser.png" alt="" class="profile-img">
+                    <div class="chat-info-container">
+                        <div class="profile-info">
+                            <h2>Tiana</h2>
+                            <p>😂😂😂</p>
+                        </div>
+                        <p>12:00</p>
                     </div>
-                    <p>12:00</p>
+                </div>
+
+                <div class="chat-profile">
+                    <img src="<?php echo BASE_URL; ?>/public/images/feedbackUser.png" alt="" class="profile-img">
+                    <div class="chat-info-container">
+                        <div class="profile-info">
+                            <h2>Tiana</h2>
+                            <p>😂😂😂</p>
+                        </div>
+                        <p>12:00</p>
+                    </div>
+                </div>
+                <div class="chat-profile">
+                    <img src="<?php echo BASE_URL; ?>/public/images/feedbackUser.png" alt="" class="profile-img">
+                    <div class="chat-info-container">
+                        <div class="profile-info">
+                            <h2>Tiana</h2>
+                            <p>😂😂😂</p>
+                        </div>
+                        <p>12:00</p>
+                    </div>
+                </div>
+                <div class="chat-profile">
+                    <img src="<?php echo BASE_URL; ?>/public/images/feedbackUser.png" alt="" class="profile-img">
+                    <div class="chat-info-container">
+                        <div class="profile-info">
+                            <h2>Tiana</h2>
+                            <p>😂😂😂</p>
+                        </div>
+                        <p>12:00</p>
+                    </div>
+                </div>
+                <div class="chat-profile">
+                    <img src="<?php echo BASE_URL; ?>/public/images/feedbackUser.png" alt="" class="profile-img">
+                    <div class="chat-info-container">
+                        <div class="profile-info">
+                            <h2>Tiana</h2>
+                            <p>😂😂😂</p>
+                        </div>
+                        <p>12:00</p>
+                    </div>
+                </div>
+                <div class="chat-profile">
+                    <img src="<?php echo BASE_URL; ?>/public/images/feedbackUser.png" alt="" class="profile-img">
+                    <div class="chat-info-container">
+                        <div class="profile-info">
+                            <h2>Tiana</h2>
+                            <p>😂😂😂</p>
+                        </div>
+                        <p>12:00</p>
+                    </div>
                 </div>
             </div>
-
         </div>
     </nav>
 </body>
