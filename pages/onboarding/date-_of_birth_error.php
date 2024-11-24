17ec05003d0a601314f4091f49d006da2e5c1bde
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2024-11-24 23:21:34 +0530

    chat profile

--- pages/onboarding/date-_of_birth_error.php
+++ pages/onboarding/date-_of_birth_error.php
@@ -43,13 +43,8 @@ session_start();
     </div>
 
     <script>
-    // Get the modal
     var modal = document.getElementById("error-panel");
-
-    // Get the button that opens the modal
     var btn = document.getElementById("errorBtn");
-
-    // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks the button, open the modal
