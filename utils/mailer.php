878560462fdf6710677e110a26c95276ec1d952c
Sandakan Nipunajith <45460443+Sandakan@users.noreply.github.com>
2024-11-25 13:04:21 +0530

    Fixed mailer and matchmaking bugs

--- utils/mailer.php
+++ utils/mailer.php
@@ -1,4 +1,8 @@
 <?php
+require_once ROOT_DIR . '/vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 function sendEmail($sendEmail, $first_name, $subject, $message)
 {
@@ -12,7 +16,7 @@ function sendEmail($sendEmail, $first_name, $subject, $message)
         $mail->Password = MAIL_PASSWORD; // sender gmail host password                          
         $mail->SMTPSecure = MAIL_ENCRYPTION;  // for encrypted connection                           
         $mail->Port = intval(MAIL_PORT);   // port for SMTP     
-
+        $mail->isHTML(true);    // Set email format to HTML
         $mail->setFrom("info@phira.com", "Phira"); // sender's email and name
         $mail->addAddress($sendEmail, $first_name);  // receiver's email and name
 
@@ -27,5 +31,3 @@ function sendEmail($sendEmail, $first_name, $subject, $message)
         echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
     }
 }
-
-sendEmail('info@phira.com', 'Quinn Hyatt', 'Test', 'Test');
